15370801f5f17daa37b3788dc6084c5027334cb5
ayan-akd <ayankumar.akd@gmail.com>
2025-03-02 00:01:50 +0600

    basic setup done

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        shine: {
+          "0%": { backgroundPosition: "200% 0" },
+          "25%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "-200% 0" },
+        },
+      },
+      animation: {
+        shine: "shine 3s ease-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
